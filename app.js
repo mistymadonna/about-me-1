7bd6cd8399298f4f662eee2853bd2e509280366c
nolana10 <nolanarendt10@gmail.com>
2016-02-11 14:03:33 -0800

    Took out all the resposes, more of Y/y N/n

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ if (answer1.toLowerCase() === 'y' || answer1.toUpperCase() === 'Y' || answer1 ==
 
 var answer2 = prompt('Is Nolans favorite sport football?');
 console.log('The users answer to Question 2 was ' + answer2);
-if (answer2.toLowerCase() === 'n'|| answer2.toUpperCase() === 'N' || answer2 === 'No' || answer2 === 'no') {
+if (answer2.toLowerCase() === 'n'|| answer2.toUpperCase() === 'N') {
   alert('Correct! My favorite sport is, and always will be hockey.');
   correctAnswers++;
   console.log(userName + ' answered Question 2 correctly!');
-} else if (answer2.toLowerCase() === 'y' || answer2.toUpperCase() === 'Y' || answer2 === 'Yes' || answer2 === 'yes') {
+} else if (answer2.toLowerCase() === 'y' || answer2.toUpperCase() === 'Y') {
   alert('Sorry, you\'re incorrect!!!');
   console.log(userName + ' got Question 2 wrong.');
 } else {
@@ -34,11 +34,11 @@ if (answer2.toLowerCase() === 'n'|| answer2.toUpperCase() === 'N' || answer2 ===
 
 var answer3 = prompt('Does Nolan have any siblings?');
 console.log('The user answer to Question 3 was ' + answer3);
-if (answer3.toLowerCase() === 'y' || answer3.toUpperCase() === 'Y' || answer3 === 'Yes' || answer3 === 'yes') {
+if (answer3.toLowerCase() === 'y' || answer3.toUpperCase() === 'Y') {
   alert('Good job! I have one sister named Madison. She does not like me very much.');
   correctAnswers++;
   console.log(userName + ' aced Question 3.');
-} else if (answer3.toLowerCase() === 'n' || answer3.toUpperCase() === 'N' || answer3 === 'No' || answer3 === 'no') {
+} else if (answer3.toLowerCase() === 'n' || answer3.toUpperCase() === 'N') {
   alert('Sorry, you\'re inccorect!!!');
   console.log(userName + ' did not quite hit the mark on Question 3.')
 } else {
@@ -48,11 +48,11 @@ if (answer3.toLowerCase() === 'y' || answer3.toUpperCase() === 'Y' || answer3 ==
 
 var answer4 = prompt('Is Nolan a Michigan State Spartans fan?');
 console.log('The user answer to Question 4 was ' + answer4);
-if (answer4.toLowerCase() === 'y' || answer4.toUpperCase() === 'Y' || answer4 === 'Yes' || answer4 === 'yes') {
+if (answer4.toLowerCase() === 'y' || answer4.toUpperCase() === 'Y') {
   alert('Correct answer! Go Green, Go White!');
   console.log(userName + ' perfected Question 4.');
   correctAnswers++;
-} else if (answer4.toLowerCase() === 'n' || answer4.toUpperCase() === 'N' || answer4 === 'No' || answer4 === 'no') {
+} else if (answer4.toLowerCase() === 'n' || answer4.toUpperCase() === 'N') {
   alert('I am dissapointed. You are incorrect!!!');
   console.log(userName + ' got Question 4 wrong. Sadly.')
 } else {
@@ -62,11 +62,11 @@ if (answer4.toLowerCase() === 'y' || answer4.toUpperCase() === 'Y' || answer4 ==
 
 var answer5 = prompt('Does Nolan like animals?');
 console.log('The user answer to Question 5 was ' + answer5);
-if (answer5.toLowerCase() === 'y' || answer5.toUpperCase() === 'Y' || answer5 === 'Yes' || answer5 === 'yes') {
+if (answer5.toLowerCase() === 'y' || answer5.toUpperCase() === 'Y') {
   alert('Correct! Nolan loves animals more than he likes people.');
   console.log(userName + ' got Question 5 correct!');
   correctAnswers++;
-} else if (answer5.toLowerCase() === 'n' || answer5.toUpperCase() === 'N' || answer5 === 'No' || answer5 === 'no') {
+} else if (answer5.toLowerCase() === 'n' || answer5.toUpperCase() === 'N') {
   alert('Incorrect! Nolan is offended that you think he does not like puppies or kittens.');
   console.log(userName + ' got Question 5 incorrect. Offending him slightly.')
 } else {
@@ -76,14 +76,14 @@ if (answer5.toLowerCase() === 'y' || answer5.toUpperCase() === 'Y' || answer5 ==
 
 var answer6 = prompt('How old is Nolan?');
 console.log('The user answer to Question 6 was ' + answer6);
-if (answer6 == 19 || answer6 === 'Nineteen' || answer6 === 'nineteen') {
+if (answer6 == 19) {
   alert('Spot on! You guessed correct!');
   console.log(userName + ' got Question 6 correct!');
   correctAnswers++;
-} else if (answer6 < 19 || answer6 === 'Sixteen' || answer6 === 'Seventeen' || answer6 === 'sixteen' || answer6 === 'seventeen' || answer6 === 'Eighteen' || answer6 === 'eighteen') {
+} else if (answer6 < 19) {
   alert('Incorrect! You guessed too young.');
   console.log(userName + ' got Question 6 wrong.')
-} else if (answer6 > 19 || answer6 === 'twenty' || answer6 === 'Twenty' || answer6 === 'Twentyone' || answer6 === 'twentyone' || answer6 === 'Twentytwo' || answer6 === 'twentytwo' || answer6 === 'Twentythree' || answer6 === 'twentythree' ) {
+} else if (answer6 > 19) {
   alert('Incorrect! You guessed too old.');
   console.log(userName + ' got Question 6 wrong.')
 }
